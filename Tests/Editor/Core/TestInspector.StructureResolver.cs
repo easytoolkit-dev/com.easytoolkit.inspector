8331ea89abee3f81d9d6b5810ed4b14086b375b5
计都 <fuyouawa@gmail.com>
2026-01-30 19:53:45 +0800

    ♻️重构：规范单测文件格式

---
+++ Tests/Editor/Core/TestInspector.StructureResolver.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using EasyToolkit.Inspector.Editor;
+using EasyToolkit.Inspector.Editor.Implementations;
+using NUnit.Framework;
+
+namespace EasyToolkit.Inspector.Editor.Tests
+{
+    [TestFixture]
+    public class TestInspector_StructureResolver
+    {
+        [Test]
+        public void Test()
+        {
+            var testInstance = new ListFieldClass
+            {
+                Integers = new List<int>()
+                {
+                    1, 2, 3
+                }
+            };
+            var tree = InspectorElements.TreeFactory.CreateTree(new object[] { testInstance }, null);
+            tree.BeginDraw();
+            tree.EndDraw();
+            var root = tree.Root;
+
+            Assert.AreEqual(1, root.LogicalChildren.Count);
+
+            var collectionElement = root.LogicalChildren[0] as ICollectionElement;
+            Assert.IsNotNull(collectionElement);
+
+            var resolver = new DefaultStructureResolverFactory().CreateResolver(collectionElement);
+            Assert.IsInstanceOf<CollectionStructureResolver<List<int>, int>>(resolver);
+            var definitions = resolver.GetChildrenDefinitions();
+            Assert.AreEqual(3, definitions.Length);
+        }
+    }
+}
