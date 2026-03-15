8591fd524c2f66d64fd7420f152643b36a1cea38
arlozen <arlozen.dev@gmail.com>
2026-03-15 17:53:04 +0800

    🐛 修复：一些细节

--- Editor/Tools/PersistentContext/PersistentContextDirectory.cs
+++ Editor/Tools/PersistentContext/PersistentContextDirectory.cs
@@ -16,9 +16,8 @@ namespace EasyToolkit.Inspector.Editor
             [DependencyProcessor]
             private ISerializationProcessor<string> _keyProcessor;
 
-            protected override void Process(string name, ref KeyValuePair<string, GlobalPersistentContext> keyValuePair, IDataFormatter formatter)
+            protected override void Process(ref KeyValuePair<string, GlobalPersistentContext> keyValuePair, IDataFormatter formatter)
             {
-                formatter.BeginMember(name);
                 using var scope = formatter.EnterObject(ValueType);
 
                 var key = keyValuePair.Key;
@@ -37,14 +36,14 @@ namespace EasyToolkit.Inspector.Editor
                 }
 
                 var valueItemType = valueType.GetGenericArgumentsRelativeTo(typeof(GlobalPersistentContext<>))[0];
-                if (SerializationProcessorFactory.GetProcessor(valueItemType) == null)
+                if (SerializationProcessorFactory.CreateProcessor(valueItemType, Context) == null)
                 {
                     keyValuePair = new KeyValuePair<string, GlobalPersistentContext>(key, null);
                     return;
                 }
 
                 object value = keyValuePair.Value;
-                var valueProcessor = SerializationProcessorFactory.GetProcessor(valueType);
+                var valueProcessor = SerializationProcessorFactory.CreateProcessor(valueType, Context);
                 valueProcessor.ProcessUntyped("Value", ref value, formatter);
 
                 if (formatter.Operation == FormatterOperation.Read)
