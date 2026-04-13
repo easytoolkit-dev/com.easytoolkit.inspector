89089b8fd8e6d4ef57d2db712ba9a7a77f4fe870
arlozen <arlozen.dev@gmail.com>
2026-04-13 21:42:26 +0800

    ✨ feat(drawers): add drawer for ReadOnlyAttribute

---
+++ Editor/Drawers/Attribute/Display/ReadOnlyAttributeDrawer.cs
@@ -0,0 +1,16 @@
+﻿using EasyToolkit.Inspector.Attributes;
+using UnityEngine;
+
+namespace EasyToolkit.Inspector.Editor
+{
+    [DrawerPriority(DrawerPriorityLevel.Super + 10)]
+    public class ReadOnlyAttributeDrawer : EasyAttributeDrawer<ReadOnlyAttribute>
+    {
+        protected override void Draw(GUIContent label)
+        {
+            GUI.enabled = false;
+            CallNextDrawer(label);
+            GUI.enabled = true;
+        }
+    }
+}
